152744fcce4da1036011ec7e2efc25b771ced588
B005255 <Trai.Tran@macys.com>
2018-01-16 14:18:16 -0500

    new feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef} from '@angular/core';
+
+declare var jQuery: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  divStyle: any;
+  changeRef: any;
+
+  constructor(private ref: ChangeDetectorRef) {
+    this.divStyle = this.defaultStyle();
+
+    this.changeRef = ref;
+  }
+
+  ngOnInit() {
+    window.addEventListener('resize', () => {
+      this.divStyle = this.defaultStyle();
+      this.changeRef.markForCheck();
+      console.log('Resize');
+      console.log('Height');
+      console.log(this.divStyle);
+    }, false);
+    console.log('Height');
+    console.log(this.divStyle);
+  }
+
+  defaultStyle() {
+    return {
+      'height': jQuery(window).height().toString() + 'px',
+      'padding': '20px'
+    };
+  }
+
+  titleFormatCircle() {
+    return '3';
+  }
+
+  getStyle() {
+    return this.divStyle;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,116 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
 
+import {AppComponent} from './app.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {BoxInfoComponent} from './common-component/box-info/box-info.component';
+import {AppRoutingModule} from './/app-routing.module';
+import {TimeLineComponent} from './time-line/time-line.component';
+import {ResumeComponent} from './resume/resume.component';
+import {FrontEndSkillComponent} from './front-end-skill/front-end-skill.component';
+import {BackEndSkillComponent} from './back-end-skill/back-end-skill.component';
+
+import {RoundProgressModule} from 'angular-svg-round-progressbar';
+import {NgCircleProgressModule} from 'ng-circle-progress';
+import { SkillChartComponent } from './skill-chart/skill-chart.component';
+import { DbSkillComponent } from './db-skill/db-skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    BoxInfoComponent,
+    TimeLineComponent,
+    ResumeComponent,
+    FrontEndSkillComponent,
+    BackEndSkillComponent,
+    SkillChartComponent,
+    DbSkillComponent
   ],
   imports: [
-    BrowserModule
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 30,
+      outerStrokeWidth: 3,
+      innerStrokeWidth: 1,
+      outerStrokeColor: '#78C000',
+      innerStrokeColor: '#C7E596',
+      animationDuration: 300,
+    }),
+    RoundProgressModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatStepperModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
